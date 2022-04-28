1d51940fedf2a4670debe0c0e4cb5771054a6d3e
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-04-28 14:32:06 +0800

    Replaced trinity-tech.cn with .io

--- backend/adapters/mydid.adapter.ts
+++ backend/adapters/mydid.adapter.ts
@@ -2,7 +2,7 @@ import { DefaultDIDAdapter } from '@elastosfoundation/did-js-sdk';
 
 export class MyDIDAdapter extends DefaultDIDAdapter {
     constructor() {
-        let resolverUrl = "https://api.trinity-tech.cn/eid";
+        let resolverUrl = "https://api.trinity-tech.io/eid";
         console.log("Using Trinity-Tech DID adapter with resolver url:", resolverUrl)
         super(resolverUrl);
     }
